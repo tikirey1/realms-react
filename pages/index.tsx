86d0f0f859c678b47f49fd4cd53b54bf21e5cf36
Michael Nolivos <michaelnolivos@gmail.com>
2022-01-04 10:57:11 -0500

    chore: path aliases
    Use same prefix as rest of Bibliotheca org

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import BridgeModal from "components/bridge/Modal";
+import BridgeModal from "~/components/bridge/Modal";
 import dynamic from "next/dynamic";
 
 const TowerWithNoSsr = dynamic(
