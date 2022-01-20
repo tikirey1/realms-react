994e1d6036b9a3c376f2762ef528791025dca1d6
micksabox <michaelnolivos@gmail.com>
2022-01-20 04:24:15 +0000

    Deploying to gh-pages from @ BibliothecaForAdventurers/realms-tower-defence@58733bcb7082c5954748452fa881b89fab3e7a22 🚀

--- src/util/messageKey.ts
+++
@@ -1,2 +0,0 @@
-export const messageKey = (nonce: string) =>
-  `Realms Tower Defence: Owning a Realms or Loot NFT, or possessing a minimum amount of $LORDS or $DIVINE grants you free $ELEMENTS to use in the game. Signing does not spend or burn any NFTs or ERC20 tokens.\n\nYou'll receive your $ELEMENTS at this StarkNet address: ${nonce}`;
